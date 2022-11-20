df086856c97473f6ccc70e4ca2dad1ff34a13baa
davfru <davide.fruci@gmail.com>
2022-11-20 10:28:26 +0100

    Create invocation-order-1.js

---
+++ invocation-order-1.js
@@ -0,0 +1,18 @@
+const { promisify } = require('util')
+
+const asyncTimeout = promisify(setTimeout)
+
+async function run() {
+  console.log('bar')
+
+  await asyncTimeout(1000)
+
+  console.log('foo')
+}
+
+run()
+
+console.log('baz');
+
+// answer
+// bar, baz, foo
